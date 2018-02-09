55e317a480b445cfefbe45be66f046efdcd9ce55
Skycocoo <luoyx2014@163.com>
2018-02-09 10:01:26 -0500

    Update texture display

--- homework 1/NYUCodebase/main.cpp
+++ homework 1/NYUCodebase/main.cpp
@@ -2,33 +2,62 @@
 // Modified by Yuxi Luo, February 9, 2018
 // Homework 1, CS3113 Game Programming
 
+// Windows req:
 #ifdef _WINDOWS
 #include <GL/glew.h>
+#define RESOURCE_FOLDER ""
 #endif
+
+#include <iostream>
+
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
-
-#include <iostream>
 #include "ShaderProgram.h"
 #include "Matrix.h"
 
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 using namespace std;
 
-#ifdef _WINDOWS
-#define RESOURCE_FOLDER ""
-#else
-#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
-#endif
 
-SDL_Window* displayWindow;
 
-ShaderProgram setup(){
-    glViewport(0, 0, 640, 360);
+// from lecture slide Jan 31, 2018
+GLuint LoadTexture(const char *filePath) {
+    int w, h, comp;
+    unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
     
+    if(image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+    
+    GLuint retTexture;
+    glGenTextures(1, &retTexture);
+    glBindTexture(GL_TEXTURE_2D, retTexture);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    stbi_image_free(image);
+    return retTexture;
+}
+
+ShaderProgram setUntextured(){
     ShaderProgram program;
     program.Load(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
     glUseProgram(program.programID);
+    
+    return program;
+}
+
+ShaderProgram setTextured(const string& filepath){
+    ShaderProgram program;
+    program.Load(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+    GLuint emojiTexture = LoadTexture((RESOURCE_FOLDER + filepath).c_str());
     
     return program;
 }
@@ -47,6 +76,25 @@ void display(ShaderProgram& program, const Matrix& modelMatrix, const Matrix& pr
     glDisableVertexAttribArray(program.positionAttribute);
 }
 
+void displayTex(ShaderProgram& program, const Matrix& modelMatrix, const Matrix& projectionMatrix, const Matrix&(viewMatrix)){
+    
+    program.SetModelMatrix(modelMatrix);
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    
+    float vertices[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    
+    float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
+}
+
 void checkKeyboard(const SDL_Event& event, bool& done){
     switch (event.type){
         case SDL_QUIT:
@@ -63,15 +111,18 @@ void checkKeyboard(const SDL_Event& event, bool& done){
 
 int main(int argc, char *argv[]){
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Homework 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+    SDL_Window* displayWindow = SDL_CreateWindow("Homework 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
+    
     
     #ifdef _WINDOWS
     glewInit();
     #endif
+    glViewport(0, 0, 640, 360);
     
-    ShaderProgram prog = setup();
+    ShaderProgram prog = setUntextured();
+    ShaderProgram progt = setTextured("starBronze.png");
     
     // matrices
     Matrix projectionMatrix;
@@ -81,6 +132,8 @@ int main(int argc, char *argv[]){
     
     SDL_Event event;
     bool done = false;
+//    float lastFrameTicks = 0.0f;
+    
     // game loop
     while (!done) {
         // check keyboard event
@@ -88,7 +141,15 @@ int main(int argc, char *argv[]){
         
         // display contents
         glClear(GL_COLOR_BUFFER_BIT);
+        
+//        float ticks = (float)SDL_GetTicks()/1000.0f;
+//        float elapsed = ticks - lastFrameTicks;
+//        lastFrameTicks = ticks;
+        
+        
         display(prog, modelMatrix, projectionMatrix, viewMatrix);
+        displayTex(progt, modelMatrix, projectionMatrix, viewMatrix);
+        
         SDL_GL_SwapWindow(displayWindow);
     }
     
